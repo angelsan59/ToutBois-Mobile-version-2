916e290b5be9989fef143f91870f656f0bfdf379
sociepka <sociepka@59013STA6101307.afpa-roubaix.local>
2017-02-23 13:28:40 +0100

    1ere version finale avec login et liste séparés

--- www/js/app.js
+++ www/js/app.js
@@ -1,74 +1,74 @@
-var url="http://localhost:8000/toutboismobPHP/"
-app=angular.module('starter', ['ionic']);
+var url = "http://localhost:80/toutboismobPHP/";
+app = angular.module('starter', ['ionic']);
 
-app.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+app.run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+        if (window.cordova && window.cordova.plugins.Keyboard) {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
 
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
+            // Don't remove this line unless you know what you are doing. It stops the viewport
+            // from snapping when text inputs are focused. Ionic handles this internally for
+            // a much nicer keyboard experience.
+            cordova.plugins.Keyboard.disableScroll(true);
+        }
+        if (window.StatusBar) {
+            StatusBar.styleDefault();
+        }
+    });
 });
 
-app.config(function($stateProvider,$urlRouterProvider){
-   $stateProvider.state("login",{
-       url:"/login/:idUser, :psw",
-       templateUrl:"templates/Login.html",
-     controller:"LoginCtrl"
-   });
-   
-   $stateProvider.state("home",{
-       url:"/home",
-       templateUrl:"templates/Home.html",
-  controller:"HomeCtrl"
-   });
-   
-   $stateProvider.state("commande",{
-       url:"/commande/:idCommande",
-       templateUrl:"templates/Commande.html",
-     controller:"CommandeCtrl"
-   });
-   
-   $stateProvider.state("listecommandes",{
-       url:"/listecommandes",
-       templateUrl:"templates/Listecommandes.html",
-      controller:"ListecommandesCtrl"
-   });
-   $urlRouterProvider.otherwise("home");
+app.config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider.state("login", {
+        url: "/login/:idUser, :psw",
+        templateUrl: "templates/Login.html",
+        controller: "LoginCtrl"
+    });
+
+    $stateProvider.state("home", {
+        url: "/home",
+        templateUrl: "templates/Home.html",
+        controller: "HomeCtrl"
+    });
+
+    $stateProvider.state("commande", {
+        url: "/commande/:idCommande",
+        templateUrl: "templates/Commande.html",
+        controller: "CommandeCtrl"
+    });
+
+    $stateProvider.state("listecommandes", {
+        url: "/listecommandes",
+        templateUrl: "templates/Listecommandes.html",
+        controller: "ListecommandesCtrl"
+    });
+    $urlRouterProvider.otherwise("home");
 });
 
 app.controller("HomeCtrl", function ($http, $scope) {
-   
+
 });
 
-app.controller('LoginCtrl', function($http, $scope) {
-   $scope.data = {};
+app.controller('LoginCtrl', function ($http, $scope) {
+    $scope.data = {};
 
-    $scope.submit = function(){
-        var link = 'http://localhost:8000/toutboismobPHP/login.php';
+    $scope.submit = function () {
+        var link = 'http://localhost:80/toutboismobPHP/login.php';
 
-        $http.post(link, {username : $scope.data.username, password : $scope.data.password }).then(function (res){
+        $http.post(link, {username: $scope.data.username, password: $scope.data.password}).then(function (res) {
             $scope.response = res.data;
         });
     };
 });
 
-app.controller('ListecommandesCtrl', function($http,$scope,$state) {
-   //creation d'un tableau liste des commandes
+app.controller('ListecommandesCtrl', function ($http, $scope, $state) {
+    //creation d'un tableau liste des commandes
     $scope.commandes = [];
     // creation d'une requete url vers le serveur
     $scope.url = url;
     $http.get(url + "listecommandes.php")
-                // data = données de la reponse
+            // data = données de la reponse
             .success(function (data) {
                 $scope.commandes = data;
             })
@@ -77,16 +77,16 @@ app.controller('ListecommandesCtrl', function($http,$scope,$state) {
             });
     $scope.chargerCommande = function (idCom) {
         $state.go("commande", {
-            idCommande: idCom,
+            idCommande: idCom
         });
-    }
+    };
 });
 
 app.controller("CommandeCtrl", function ($http, $scope, $stateParams) {
     $scope.idCommande = $stateParams.idCommande;
-   
+
     $scope.commande = [];
-    $http.get(url + "detailscom.php?idCommande=" + $scope.idCommande )
+    $http.get(url + "detailscom.php?idCommande=" + $scope.idCommande)
             .success(function (data) {
                 $scope.commande = data;
             })
